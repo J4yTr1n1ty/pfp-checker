b295c386e33467bba033d705ce8b1f57925e0c77
Jay <julian.graf@projectchaos.ch>
2024-05-13 12:11:36 +0200

    feat(uploader): Added own uploading capabilities so you can actually look at previous profile pictures

--- src/util/objects.rs
+++ src/util/objects.rs
@@ -1,5 +1,19 @@
+use serde::{Deserialize, Serialize};
+
 pub struct ProfilePictureEntry {
     pub title: String,
     pub content: String,
     pub inline: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ImgBBResponse {
+    pub data: ImgBBData,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ImgBBData {
+    pub id: String,
+    pub url: String,
+    pub display_url: String,
 }
