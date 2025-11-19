28c12f32d4d508d0a19999e1b6b03a123b8adbf8
Jay <jay@projectchaos.ch>
2025-11-19 17:48:11 +0100

    fix: remove unused duplicate ImgBB structs
    - ImgBBResponse and ImgBBData were duplicated in objects.rs
    - The actual implementations are in util/external/imgbb.rs
    - This fixes the dead code warnings causing CI to fail

--- src/util/objects.rs
+++ src/util/objects.rs
@@ -1,19 +1,5 @@
-use serde::{Deserialize, Serialize};
-
 pub struct EmbedEntry {
     pub title: String,
     pub content: String,
     pub inline: bool,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ImgBBResponse {
-    pub data: ImgBBData,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ImgBBData {
-    pub id: String,
-    pub url: String,
-    pub display_url: String,
 }
